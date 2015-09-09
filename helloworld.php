55bbe452d50e778a529290c6da0ebc3f3e951003
Merilin Takk <meritak@greeny.cs.tlu.ee>
2015-09-09 11:33:37 +0300

    new file

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,25 @@
 	echo $first_name." ".$last_name;
 
 ?>
+<br>
+<?php
+
+    $age = 5;
+	
+	//testib loogikat, juhul kui vanus on väiksem 
+	//kui 18 siis kirjuta "alaealine", muul juhul "täisealine"
+    
+	//if's sisse loogikaehe
+	if($age < 18) {
+		//tõene
+		
+		echo "alaealine";
+		
+    } else {
+		//väär
+		
+		echo "täisealine";
+		
+	}
+
+?>	

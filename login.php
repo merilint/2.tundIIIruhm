6717942a40436a0bb1a848ea454305061395bde5
Merilin Takk <meritak@greeny.cs.tlu.ee>
2015-09-09 12:39:48 +0300

    new

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+    //login.php
+	
+	
+?>
+<html>
+<head>
+    <title>Login page</title>
+</head>
+<body>
+    <h2>Log in</h2>
+	<from action="user_from.php" method="post">
+	<input name="email" type="email" placeholder="E-post"> <br><br>
+	<input name="password" type="password" placeholder="Parool" > <br><br>
+	<input type ="submit" value="logi sisse">
+	</from>
+	
+	<h2>Create user</h2>
+	
+</body
+</html>

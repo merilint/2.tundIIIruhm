e600c6b234ed11c7dcb33cf29466c29eebfb2e51
Merilin Takk <meritak@greeny.cs.tlu.ee>
2015-09-09 12:13:13 +0300

    new file

--- helloworld.php
+++ helloworld.php
@@ -7,7 +7,7 @@
 <br>
 <?php
 
-    $age = 5;
+    $age = 3;
 	
 	//testib loogikat, juhul kui vanus on väiksem 
 	//kui 18 siis kirjuta "alaealine", muul juhul "täisealine"
@@ -26,3 +26,22 @@
 	}
 
 ?>	
+<br>
+<?php
+    //vastavalt vanusele trükime nii mitu korda välja sõna "palju"
+	
+    for ($i = 0; $i < $age; $i = $i + 1){
+		//see, mida korratakse
+		echo "palju ";
+	
+	}
+	
+	echo "õnne!";
+		
+	
+	?>
+	<br>
+<?php
+    // kuupäev - date
+    echo date("l, j F Y e");
+?>
